affdbe35ab81b406b9ae91ac4c79c0b54d4412f3
Aomine7777 <141518740+Aomine7777@users.noreply.github.com>
2024-04-17 00:11:01 +0300

    add validation for id Planet

--- src/main/java/entities/Planet.java
+++ src/main/java/entities/Planet.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
 
 @Data
 @NoArgsConstructor
@@ -15,6 +19,9 @@ import lombok.NoArgsConstructor;
 @Table(name = "planet")
 public class Planet {
     @Id
+    @NotNull
+    @Length(min = 2, max = 10)
+    @Pattern(regexp = "[A-Z0-9]+")
     private String id;
     @Column(nullable = false, length = 500)
     private String name;
